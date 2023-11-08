e9f6eee7730f70a41664d69fcfadd76c7cc00a18
Anurag_Ponnada <apanuragp7@gmail.com>
2023-11-08 23:19:41 +0530

    fix
    ->adjusted height and width of cards so that they're all uniform
    ->Included other info from api, such as Delivery time,areaname,cost for Two

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,24 @@
 
 import url from "../utils/constants";
 const Cards = (props)=>{
-    const{cloudinaryImageId,id,name,cuisines,avgRating,locality} = props?.cardData;
+    const{cloudinaryImageId,id,name,cuisines,avgRating,locality,areaname,costForTwo,sla} = props?.cardData;
     var dummyKey = Object.keys(props.cardData).length; //just to handle unique key problem in cuisines which uses li
     // console.log(typeof dummyKey)
     return(
         <div className="cards-component">
-            <img style={{height:"200px",width:"200px"}} src={url+cloudinaryImageId}></img>
-            <h1>{id}</h1> 
+            <img style={{height:"200px",width:"250px"}} src={url+cloudinaryImageId}></img>
+            {/* <h1>{id}</h1>  */}
             <h1>{name}</h1>
-            <h1>cuisines</h1>
+            
             <ul>
-               {cuisines.map((each_cuisine)=>{return <li key={dummyKey = dummyKey -1}>{each_cuisine}</li>})}
+                <li style={{fontStyle:"oblique"}}><h2>cuisines</h2></li>
+               {cuisines.map((each_cuisine)=>{return <li key={dummyKey = dummyKey -1}><h2>{each_cuisine}</h2></li>})}
             </ul>
-            <h1>{avgRating}</h1>
-            <h1>{locality}</h1>
+            <h1>Rating : {avgRating}</h1>
+            <h1>🗺️ {locality}</h1>
+            <h1>{areaname}</h1>
+            <h1>Cost for Two: 💰{costForTwo}</h1>
+            <h1>Delivery Time: ⌛{sla.deliveryTime}</h1>
         </div>
     )
 }
